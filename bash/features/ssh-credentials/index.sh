0b25921de28eb0c49b473c507cd1576df1faf026
Andrew Smith <andrew@andrew.codes>
2018-02-08 07:50:45 -0500

    Support multiple SSH keys

--- bash/features/ssh-credentials/index.sh
+++ bash/features/ssh-credentials/index.sh
@@ -4,24 +4,4 @@ if [ $SSH_CREDENTIALS -ne 1 ]; then
   return 0;
 fi
 
-SSH_ENV=$HOME/.ssh/environment
-
-function start_agent {
-  echo "Initialising new SSH agent..."
-  /usr/bin/ssh-agent | sed 's/^echo/#echo/' > ${SSH_ENV}
-  echo succeeded
-  chmod 600 ${SSH_ENV}
-  . ${SSH_ENV} > /dev/null
-  /usr/bin/ssh-add;
-}
-
-# Source SSH settings, if applicable
-if [ -f "${SSH_ENV}" ]; then
-  . ${SSH_ENV} > /dev/null
-  #ps ${SSH_AGENT_PID} doesn't work under cywgin
-  ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-    start_agent;
-   }
-else
-  start_agent;
-fi
+find ~/.ssh -maxdepth 1 -type f ! -name "*.*" ! -name "*known_hosts" -exec keychain --eval --agents ssh --inherit any {} \;
