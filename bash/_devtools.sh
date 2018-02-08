be2d202134ad28681f098fe1de5ca7e49a198859
Andrew Smith <andrew@andrew.codes>
2018-02-08 07:58:53 -0500

    Remove code

--- bash/_devtools.sh
+++ bash/_devtools.sh
@@ -27,12 +27,3 @@ export NVM_DIR="$HOME/.nvm"
 source $(brew --prefix nvm)/nvm.sh
 
 export PATH="/bin:$PATH"
-
-if test -f ~/.gnupg/.gpg-agent-info -a -n "$(pgrep gpg-agent)"; then
-  source ~/.gnupg/.gpg-agent-info
-  export GPG_AGENT_INFO
-  GPG_TTY=$(tty)
-  export GPG_TTY
-else
-  eval $(gpg-agent --daemon ~/.gnupg/.gpg-agent-info)
-fi
