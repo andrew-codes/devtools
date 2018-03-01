0384919d1736849f6ecd8ae05fec83b567a7e1f9
Andrew Smith <andrew@andrew.codes>
2018-03-01 09:30:28 -0500

    Better OSX versus Windows support

--- bash/_devtools.sh
+++ bash/_devtools.sh
@@ -21,9 +21,11 @@ source ${DEVTOOLS_HOME}/bash/features/hub/index.sh
 source ${DEVTOOLS_HOME}/bash/features/project-finder/index.sh
 
 # [GitX](http://gitx.frim.nl/user_manual.html); OSX only
-[ ${IS_OSX} ] && [ -f /usr/local/bin/gitx ] && alias gui=gitx
+[ $IS_OSX -eq 1 ] && [ -f /usr/local/bin/gitx ] && alias gui=gitx
 
 export NVM_DIR="$HOME/.nvm"
-source $(brew --prefix nvm)/nvm.sh
+if [ $IS_OSX -eq 1 ]; then
+  source $(brew --prefix nvm)/nvm.sh
+fi
 
 export PATH="/bin:$PATH"
--- bash/features/ssh-credentials/index.sh
+++ bash/features/ssh-credentials/index.sh
@@ -4,4 +4,6 @@ if [ $SSH_CREDENTIALS -ne 1 ]; then
   return 0;
 fi
 
-find ~/.ssh -maxdepth 1 -type f ! -name "*.*" ! -name "*known_hosts" -exec keychain --eval --agents ssh --inherit any {} \;
+if [ $IS_OSX -eq 1 ]; then
+  find ~/.ssh -maxdepth 1 -type f ! -name "*.*" ! -name "*known_hosts" -exec keychain --eval --agents ssh --inherit any {} \;
+fi
