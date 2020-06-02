fd6365877b991692adc7b5a1fbc54713810db38e
Andrew Smith <andrew@andrew.codes>
2020-06-02 19:00:10 -0400

    more vs code extensions

--- vscode.extensions.sh
+++ vscode.extensions.sh
@@ -23,9 +23,9 @@ code --install-extension silvenon.mdx
 code --install-extension znck.grammarly
 code --install-extension xyc.vscode-mdx-preview
 
-# ==================
+# =============================
 # JS, Refactoring, Code Metrics
-# ==================
+# =============================
 code --install-extension cmstead.jsrefactor
 code --install-extension capaj.vscode-exports-autocomplete
 code --install-extension kisstkondoros.vscode-codemetrics
@@ -34,18 +34,19 @@ code --install-extension wix.vscode-import-cost
 # =======
 # Testing
 # =======
-code --install-extension hbenl.vscode-test-explorer
-code --install-extension Orta.vscode-jest
+# code --install-extension hbenl.vscode-test-explorer
+# code --install-extension Orta.vscode-jest
 code --install-extension andrew-codes.jest-snippets
 
 # Requires WallabyJS license: https://wallabyjs.com/
 code --install-extension WallabyJs.wallaby-vscode
 
-# ======
-# eslint
-# ======
+# ===================
+# Lint and Formatting
+# ===================
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension drKnoxy.eslint-disable-snippets
+code --install-extension esbenp.prettier-vscode
 
 # ==================
 # Package Management
@@ -55,6 +56,11 @@ code --install-extension pflannery.vscode-versionlens
 code --install-extension christian-kohler.npm-intellisense
 code --install-extension jasonnutter.search-node-modules
 
+# ================
+# Database Support
+# ================
+code --install-exetension ckolkman.vscode-postgres
+
 # ==========
 # Presenting
 # ==========
@@ -62,9 +68,9 @@ code --install-extension jspolancor.presentationmode
 code --install-extension pnp.polacode
 code --install-extension vsls-contrib.codetour
 
-# ==========
+# =================
 # React, CSS-in-JSS
-# ==========
+# =================
 code --install-extension paulmolluzzo.convert-css-in-js
 code --install-extension OfHumanBondage.react-proptypes-intellisense
 
@@ -79,3 +85,5 @@ code --install-extension WallabyJs.quokka-vscode
 
 code --install-extension alefragnani.Bookmarks
 code --install-extension mikestead.dotenv
+
+code --install-extension keesschollaart.vscode-home-assistant
