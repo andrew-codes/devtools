d8820ae8aff8685093c23089cb82742c72cbd1fd
Andrew Smith <andrew@andrew.codes>
2020-10-28 17:23:01 -0400

    minor updates

--- install.osx.sh
+++ install.osx.sh
@@ -1,36 +1,35 @@
-echo "Install homebrew..."
+#!/usr/bin/env bash
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-brew update
 
-echo "Installing security items..."
 brew cask install 1password
-brew cask install keybase
-
-echo "Installing Internet items..."
-brew cask install google-chrome
-brew cask install discord
-
-echo "Installing productivity items..."
 brew cask install alfred
-brew cask install iterm2
-brew cask install docker
 brew cask install bettertouchtool
-# brew cask install snagit
-
-echo "Installing development items..."
-brew cask install visual-studio-code
-brew install nvm
-echo "Installing scm items..."
-brew install git
-brew install github/gh/gh
-brew cask install rowanj-gitx
-
-echo "Installing gaming items..."
+brew cask install dash
+brew cask install discord
+brew cask install docker
+brew cask install firefox
+brew cask install google-chrome
+brew cask install grammarly
+brew cask install iterm2
+brew cask install keybase
+brew cask install kindle
+brew cask install logitech-presentation
+brew cask install microsoft-office
+brew cask install microsoft-teams
 brew cask install parsec
+brew cask install pdf-expert
+brew cask install pliim
+brew cask install rowanj-gitx
+brew cask install slack
+brew cask install snagit
+brew cask install spotify
+brew cask install visual-studio-code
+brew install gh
+brew install keychain
+brew install nvm
 
-echo "Setting up vscode extenions..."
+## Setup devtools for bash.
+source ./add-devtools-to-bash_profile.sh
+
 source ./vscode.extensions.sh
-
-echo "Installing balena tools"
-brew install balena-cli
-brew cask install balenaetcher
