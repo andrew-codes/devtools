c48ded595543917f531f6b8126e241a8a2061bf4
Andrew Smith <andrew@andrew.codes>
2020-11-30 12:24:33 -0500

    install missing tools

--- install.osx.sh
+++ install.osx.sh
@@ -2,6 +2,8 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+xcode-select --install
+
 brew cask install 1password
 brew cask install alfred
 brew cask install bettertouchtool
@@ -19,7 +21,7 @@ brew cask install logitech-presentation
 brew cask install microsoft-office
 brew cask install microsoft-teams
 brew cask install parsec
- brew install pinentry-mac
+brew install pinentry-mac
 brew cask install pdf-expert
 brew cask install pliim
 brew cask install rowanj-gitx
@@ -31,6 +33,8 @@ brew install gh
 brew install gpg
 brew install keychain
 brew install nvm
+brew install gcc
+brew install yarn
 
 ## Setup devtools for bash.
 source ./add-devtools-to-bash_profile.sh
