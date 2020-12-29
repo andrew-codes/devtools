b8397b33fb58424b9e8648c0b76d5034ba335955
Andrew Smith <andrewsmith@microsoft.com>
2020-12-29 13:16:52 -0500

    windows installation script

---
+++ install.win.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+## Setup devtools for bash.
+source ./add-devtools-to-bash_profile.sh
+
+source ./vscode.extensions.sh
+
+# Setup GPG keys from keybase
+source ./gpg-keybase.sh
