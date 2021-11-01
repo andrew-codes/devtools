60b0411fd9206092d3f8846279c7874432a0d010
Andrew Smith <andrew@andrew.codes>
2021-11-01 15:36:24 -0400

    syntax updates

--- bash_features.sh
+++ bash_features.sh
@@ -2,10 +2,9 @@
 # Path variables
 # ==============
 DEV_HOME=$HOME/developer
-REPO_HOME=${DEV_HOME}/repos
-DEVTOOLS_HOME=${REPO_HOME}/devtools
-TOOLS_HOME=${DEV_HOME}/tools/
-
+REPO_HOME=$DEV_HOME/repos
+DEVTOOLS_HOME=$REPO_HOME/devtools
+TOOLS_HOME=$DEV_HOME/tools/
 
 # Enable/Disable Features
 # =======================
