4602f2711d66b2e33f29b1a2f893c38664025934
Andrew Smith <andrew@andrew.codes>
2023-12-04 09:00:44 -0500

    enable osx vscode bindings as setting

--- software/vscode/windows/setup.sh
+++ software/vscode/windows/setup.sh
@@ -1,6 +1,7 @@
 wingetInstall Microsoft.VisualStudioCode
-wingetInstall Microsoft.PowerToys
 
-echo -e "Remember to map the left Windows key to Left Control in PowerToys Keyboard Manager. See https://learn.microsoft.com/en-us/windows/powertoys/keyboard-manager for more information."
-
-cp ./key-bindings.jsonc ~/AppData/Roaming/Code/User/keybindings.json
+if [ "$DEVTOOLS_VSCODE_OSX_KEYBINDINGS" == "true" ]; then
+  wingetInstall Microsoft.PowerToys
+  echo -e "Remember to map the left Windows key to Left Control in PowerToys Keyboard Manager. See https://learn.microsoft.com/en-us/windows/powertoys/keyboard-manager for more information."
+  cp ./key-bindings.jsonc ~/AppData/Roaming/Code/User/keybindings.json
+fi
