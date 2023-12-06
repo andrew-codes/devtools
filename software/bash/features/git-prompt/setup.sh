27e0bbc56d6ea504e86d0f8a009c3564a63fcbfc
Andrew Smith <andrew@andrew.codes>
2023-12-06 10:43:05 -0500

    prompt setup correctly

--- software/bash/features/git-prompt/setup.sh
+++ software/bash/features/git-prompt/setup.sh
@@ -1,9 +1,8 @@
 curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh >$DEVTOOLS_BASH_TOOLS_HOME/git-prompt.sh
 
-impl=$(cat $DEVTOOLS_BASH_TOOLS_HOME/git-prompt.sh)
 echo -e "# <DEVTOOLS>
 # Git Prompt
 # ==========
-$impl
+source $TOOLS_HOME/git-prompt.sh
 # </DEVTOOLS>
 " >>~/.bash_profile
