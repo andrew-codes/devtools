5e5626fb2a19f9bafcf758dac5b78025f95a5508
Andrew Smith <andrew@andrew.codes>
2023-12-06 09:30:35 -0500

    dev container bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,23 @@
 
 # This file is exclusively for setting up devtools in a dev container/codespace.
 
-export DEV_HOME=$HOME
-export REPO_HOME=$DEV_HOME
-export DEVTOOLS_HOME=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-export TOOLS_HOME=$DEV_HOME/tools/
+# Bash
+export DEVTOOLS_BASH=true
+export DEVTOOLS_BASH_DEV_HOME=$HOME
+export DEVTOOLS_BASH_REPO_HOME=$HOME
+export DEVTOOLS_BASH_DEVTOOLS_HOME=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+export DEVTOOLS_BASH_TOOLS_HOME=$HOME/tools
+export DEVTOOLS_BASH_TOOLS_BIN_HOME=$HOME/tools/bin
+# Feature toggles
+export DEVTOOLS_BASH_FEATURES_COMMANDS=true
+export DEVTOOLS_BASH_FEATURES_GIT_EXTENDED=true
+export DEVTOOLS_BASH_FEATURES_GIT_COMPLETION=true
+export DEVTOOLS_BASH_FEATURES_GIT_PROMPT=true
+# Use only one of the following: ---
+export DEVTOOLS_BASH_FEATURES_USE_1PASSWORD_AGENT=false
+export DEVTOOLS_BASH_FEATURES_USE_SSH_AGENT=false
+# ---
+
+# Git
+export DEVTOOLS_GIT=false
+export DEVTOOLS_GIT_FEATURES_CONFIG=false
