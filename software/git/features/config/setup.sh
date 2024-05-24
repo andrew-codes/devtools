f06f969adca444e8f55fcc20803ac3de9d409828
Andrew Smith <andrew@andrew.codes>
2024-05-24 16:06:38 -0400

    fix: git signing key

--- software/git/features/config/setup.sh
+++ software/git/features/config/setup.sh
@@ -9,8 +9,10 @@ fi
 if [ ! "$DEVTOOLS_GIT_FEATURES_CONFIG_NAME" == "" ]; then
   git config --global user.name "$(DEVTOOLS_GIT_FEATURES_CONFIG_NAME)"
 fi
+if [ ! "$DEVTOOLS_GIT_FEATURES_CONFIG_SIGNINGKEY" == "" ]; then
+  git config --global user.signingkey "$(DEVTOOLS_GIT_FEATURES_CONFIG_SIGNINGKEY)"
+fi
 
-git config --global user.signingkey "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDHGX0BrMpr5mm/maYDNJJhBrn1lvjgy+9//ufn+QV5rC8MQpkakTKi3qzEQ22Xw4bOPb59C80TkH8T6ur4Ygb0oPMhFCQoBpd1rabQCIISSwi+I4bth58h8Jl/tXdiNclfTyPHNBPxRTjOGG9Op+Zu8EQtd4QUinf3iFFKJ4Wyk9cuHbKGYkhKnQG/u1LD+IJ6y2pt4Cdh2hnO2HIsSKOp6djx8zuCOMyguN1giFsa4gmd3/TcNO/O/p6G1Xs3v1H9KWWtXVL0gRRd1NTbnbqyuBmlBu2wKWVbznlf7Jjkb0asophnHBSsIcwJU079YGWfCVeZ0eoq/goDcI2Nj+FkNTJsJxuOwCUCBCikPZwUstU1cRAhTP72pu08ZQXM/B+uF2lDCLVu+Kui2bZQbOjNGunRnsFfer7XGpfqIeaYd8zJNFQPQIoE5N+iRMRQ/M1NHY1+E0TtdxWIi3pN11r7d9SLV4XYYdU5OgZFBKeQXULY5tKYG/ZMQj0MPmpksZ8="
 git config --global push.default = simple
 git config --global branch.autosetuprebase = always
 git config --global core.autocrlf = false
