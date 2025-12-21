e04b7910e5b3e03eb0f35615dfa070d59d8cdc1c
Andrew Smith <andrew@andrew.codes>
2025-12-20 20:54:39 -0500

    fix 1password setup errors

--- software/bash/features/use-1password-agent/osx/setup.sh
+++ software/bash/features/use-1password-agent/osx/setup.sh
@@ -1,2 +1,6 @@
-brewInstall 1password --cask
+if [ ! -d "/Applications/1Password.app" ]; then
+  brewInstall 1password --cask
+else
+  echo "1Password is already installed, skipping installation"
+fi
 echo "Please follow directions to turn on 1Password SSH Agent; https://developer.1password.com/docs/ssh/get-started#step-3-turn-on-the-1password-ssh-agent"
